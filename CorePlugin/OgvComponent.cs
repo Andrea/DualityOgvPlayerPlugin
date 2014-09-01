33a7ecbc0df1471a9bc90620fdfc6e1cff7a3171
Andrea <roundcrisis@gmail.com>
2014-09-01 17:37:36 +0100

    Plays video and sounds. Partially plays sounds via Fmod

--- CorePlugin/OgvComponent.cs
+++ CorePlugin/OgvComponent.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading;
 using Duality;
 using Duality.Components;
@@ -91,6 +93,8 @@ namespace OgvPlayer
 			TextureTwo = new ContentRef<Texture>(new Texture(Width / 2, Height / 2, format: PixelInternalFormat.Luminance));
 			TextureThree = new ContentRef<Texture>(new Texture(Width / 2, Height / 2, format: PixelInternalFormat.Luminance));
 
+			FmodTheoraStream.Init();
+
 			// FIXME: This is a part of the Duration hack!
 			Duration = TimeSpan.MaxValue;
 		}
@@ -180,6 +184,30 @@ namespace OgvPlayer
 				videoStream = nextFrame;
 				_nextVideo = TheoraPlay.getVideoFrame(videoStream);
 			}
+
+			const int BUFFER_SIZE = 4096 * 2;
+
+			// Store our abstracted buffer into here.
+			var data = new List<float>();
+
+			TheoraPlay.THEORAPLAY_AudioPacket currentAudio;
+			currentAudio.channels = 0;
+			currentAudio.freq = 0;
+
+//			while (TheoraPlay.THEORAPLAY_availableAudio(theoraDecoder) == 0) ;
+			
+			while (data.Count < BUFFER_SIZE && TheoraPlay.THEORAPLAY_availableAudio(theoraDecoder) > 0)
+			{
+				var audioPtr = TheoraPlay.THEORAPLAY_getAudio(theoraDecoder);
+				currentAudio = TheoraPlay.getAudioPacket(audioPtr);
+				data.AddRange(TheoraPlay.getSamples(
+						currentAudio.samples,
+						currentAudio.frames * currentAudio.channels)
+				);
+				TheoraPlay.THEORAPLAY_freeAudio(audioPtr);
+			}
+
+			FmodTheoraStream.Stream(data.ToArray());
 		}
 
 		public override void Draw(IDrawDevice device)
@@ -248,6 +276,111 @@ namespace OgvPlayer
 		public override float BoundRadius
 		{
 			get { return float.MaxValue; }
+		}
+	}
+
+	public class FmodTheoraStream
+	{
+		private static Fmod.System system = null;
+		private static Fmod.Sound sound = null;
+		private static Fmod.Channel channel = null;
+		private static Fmod.CREATESOUNDEXINFO createsoundexinfo = new Fmod.CREATESOUNDEXINFO();
+		private static bool soundcreated = false;
+		private static Fmod.MODE mode = (Fmod.MODE._2D | Fmod.MODE.DEFAULT | Fmod.MODE.OPENUSER | Fmod.MODE.LOOP_NORMAL | Fmod.MODE.HARDWARE);
+		private static float[] _buffer = new float[0];
+		private static object _syncObject = new object();
+
+		public static void Init()
+		{
+			uint version = 0;
+			Fmod.RESULT result;
+			uint channels = 2, frequency = 48000;
+
+			/*
+				Create a System object and initialize.
+			*/
+			result = Fmod.Factory.System_Create(ref system);
+//			ERRCHECK(result);
+			result = system.getVersion(ref version);
+//			ERRCHECK(result);
+//			if (version < Fmod.VERSION.number)
+//			{
+//				MessageBox.Show("Error!  You are using an old version of Fmod " + version.ToString("X") + ".  This program requires " + Fmod.VERSION.number.ToString("X") + ".");
+//				Application.Exit();
+//			}
+			result = system.init(32, Fmod.INITFLAGS.NORMAL, (IntPtr)null);
+//			ERRCHECK(result);
+
+			createsoundexinfo.cbsize = Marshal.SizeOf(createsoundexinfo);
+			createsoundexinfo.fileoffset = 0;
+			createsoundexinfo.length = frequency * channels * 2 * 2;
+			createsoundexinfo.numchannels = (int)channels;
+			createsoundexinfo.defaultfrequency = (int)frequency;
+			createsoundexinfo.format = Fmod.SOUND_FORMAT.PCMFLOAT;
+			createsoundexinfo.pcmreadcallback += PcmReadCallback;
+			createsoundexinfo.dlsname = null;
+
+			if (!soundcreated)
+			{
+				result = system.createSound(
+					(string)null,
+					(mode | Fmod.MODE.CREATESTREAM),
+					ref createsoundexinfo,
+					ref sound);
+
+				soundcreated = true;
+			}
+			system.playSound(Fmod.CHANNELINDEX.FREE, sound, false, ref channel);
+		}
+
+		public static void Stream(float[] data)
+		{
+			lock (_syncObject)
+			{
+				var destinationIndex = _buffer.Length;
+				Array.Resize(ref _buffer, _buffer.Length + data.Length);
+				Array.Copy(data, 0, _buffer, destinationIndex, data.Length);
+			}
+		}
+
+		private static Fmod.RESULT PcmReadCallback(IntPtr soundraw, IntPtr data, uint datalen)
+		{
+			unsafe
+			{
+				uint count;
+
+				lock (_syncObject)
+				{
+					if (_buffer.Length == 0)
+						return Fmod.RESULT.OK;
+
+					var stereo32BitBuffer = (float*)data.ToPointer();
+
+					for (count = 0; count < (datalen >> 2); count+=2)
+					{
+						*stereo32BitBuffer++ = _buffer[count];
+						*stereo32BitBuffer++ = _buffer[count + 1];
+					}
+
+					var temp = new float[_buffer.Length - (datalen >> 2)];
+					Array.Copy(_buffer, (datalen >> 2), temp, 0, _buffer.Length - (datalen >> 2));
+					_buffer = temp;
+				}
+
+//				short* stereo16bitbuffer = (short*)data.ToPointer();
+//
+//				for (count = 0; count < (datalen >> 2); count++)        // >>2 = 16bit stereo (4 bytes per sample)
+//				{
+//					*stereo16bitbuffer++ = (short)(Math.Sin(t1) * 32767.0f);    // left channel
+//					*stereo16bitbuffer++ = (short)(Math.Sin(t2) * 32767.0f);    // right channel
+//
+//					t1 += 0.01f + v1;
+//					t2 += 0.0142f + v2;
+//					v1 += (float)(Math.Sin(t1) * 0.002f);
+//					v2 += (float)(Math.Sin(t2) * 0.002f);
+//				}
+			}
+			return Fmod.RESULT.OK;
 		}
 	}
 }
