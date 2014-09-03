b0cccdb78646aea01f787b141516aee31a33ed56
Andrea <roundcrisis@gmail.com>
2014-09-03 15:22:21 +0100

    Adding some error checking

--- CorePlugin/FmodTheoraStream.cs
+++ CorePlugin/FmodTheoraStream.cs
@@ -1,93 +1,98 @@
 using System;
 using System.Runtime.InteropServices;
+using Duality;
 using OgvPlayer.Fmod;
 
 namespace OgvPlayer
 {
-	public class FmodTheoraStream
-	{
-		const int BufferSize = 1024768;
+    public class FmodTheoraStream
+    {
+        const int BufferSize = 1024768;
 
-		private static Fmod.System _system;
-		private static Sound _sound;
-		private static Channel _channel;
-		private static CREATESOUNDEXINFO _createsoundexinfo;
-		private static bool _soundcreated;
-		private static MODE mode = (Fmod.MODE._2D | Fmod.MODE.DEFAULT | Fmod.MODE.OPENUSER | Fmod.MODE.LOOP_NORMAL | Fmod.MODE.HARDWARE);
-		
-		private static CircularBuffer<float> _circularBuffer;
-		private static object _syncObject = new object();
+        private static Fmod.System _system;
+        private static Sound _sound;
+        private static Channel _channel;
+        private static CREATESOUNDEXINFO _createsoundexinfo;
+        private static bool _soundcreated;
+        private static MODE mode = (Fmod.MODE._2D | Fmod.MODE.DEFAULT | Fmod.MODE.OPENUSER | Fmod.MODE.LOOP_NORMAL | Fmod.MODE.HARDWARE);
 
-		public static void Init()
-		{
-			uint version = 0;
-			RESULT result;
-			const uint channels = 2;
-			const uint frequency = 48000;
-			_circularBuffer = new CircularBuffer<float>(BufferSize, true);
-			
-			result = Factory.System_Create(ref _system);
-			//			ERRCHECK(result);
-			result = _system.getVersion(ref version);
-			//			ERRCHECK(result);
-			//			if (version < Fmod.VERSION.number)
-			//			{
-			//				MessageBox.Show("Error!  You are using an old version of Fmod " + version.ToString("X") + ".  This program requires " + Fmod.VERSION.number.ToString("X") + ".");
-			//				Application.Exit();
-			//			}
-			result = _system.init(32, INITFLAGS.NORMAL, (IntPtr)null);
-			//			ERRCHECK(result);
+        private static CircularBuffer<float> _circularBuffer;
+        private static object _syncObject = new object();
 
-			_createsoundexinfo.cbsize = Marshal.SizeOf(_createsoundexinfo);
-			_createsoundexinfo.fileoffset = 0;
-			_createsoundexinfo.length = frequency * channels * 2 * 2;
-			_createsoundexinfo.numchannels = (int)channels;
-			_createsoundexinfo.defaultfrequency = (int)frequency;
-			_createsoundexinfo.format = SOUND_FORMAT.PCMFLOAT;
-			_createsoundexinfo.pcmreadcallback += PcmReadCallback;
-			_createsoundexinfo.dlsname = null;
+        public static void Init()
+        {
+            uint version = 0;
+            var result = RESULT.ERR_UPDATE;
+            const uint channels = 2;
+            const uint frequency = 48000;
+            _circularBuffer = new CircularBuffer<float>(BufferSize, true);
+            try
+            {
 
-			if (!_soundcreated)
-			{
-				result = _system.createSound( (string)null, (mode | MODE.CREATESTREAM),ref _createsoundexinfo,ref _sound);
-				_soundcreated = true;
-			}
-			_system.playSound(CHANNELINDEX.FREE, _sound, false, ref _channel);
-		}
 
-		public static void Stream(float[] data)
-		{
-			lock (_syncObject)
-			{
-				
-				_circularBuffer.Put(data);
-			}
-		}
+                result = Factory.System_Create(ref _system);
+                result = _system.getVersion(ref version);
+                result = _system.init(32, INITFLAGS.NORMAL, (IntPtr)null);
 
-		private static RESULT PcmReadCallback(IntPtr sounDraw, IntPtr data, uint datalen)
-		{
-			unsafe
-			{
-				uint count; //Does this need to be outside the lock? AM
+                _createsoundexinfo.cbsize = Marshal.SizeOf(_createsoundexinfo);
+                _createsoundexinfo.fileoffset = 0;
+                _createsoundexinfo.length = frequency * channels * 2 * 2;
+                _createsoundexinfo.numchannels = (int)channels;
+                _createsoundexinfo.defaultfrequency = (int)frequency;
+                _createsoundexinfo.format = SOUND_FORMAT.PCMFLOAT;
+                _createsoundexinfo.pcmreadcallback += PcmReadCallback;
+                _createsoundexinfo.dlsname = null;
 
-				lock (_syncObject)
-				{
-					
-					if (_circularBuffer.Size == 0)
-						return RESULT.OK;
+                if (!_soundcreated)
+                {
+                    result = _system.createSound((string)null, (mode | MODE.CREATESTREAM), ref _createsoundexinfo, ref _sound);
+                    _soundcreated = true;
+                }
+            }
+            catch (Exception exception)
+            {
+                Log.Editor.WriteWarning("Problem initialising the audio part of the video player.{0}. Error: {1}", result, exception.Message);
+            }
+            if (result != RESULT.OK)
+            {
+                Log.Editor.WriteWarning("Problem initialising the audio part of the video player.{0}. No exceptions", result);
+            }
+            _system.playSound(CHANNELINDEX.FREE, _sound, false, ref _channel);
+        }
 
-					var stereo32BitBuffer = (float*)data.ToPointer();
-					for (count = 0; count < (datalen >> 2); count++) //WTF does this do AM
-					{
-						if (_circularBuffer.Size == 0)
-							break;
+        public static void Stream(float[] data)
+        {
+            lock (_syncObject)
+            {
 
-						*stereo32BitBuffer++ = _circularBuffer.Get();
-					}
-					
-				}
-			}
-			return RESULT.OK;
-		}
-	}
+                _circularBuffer.Put(data);
+            }
+        }
+
+        private static RESULT PcmReadCallback(IntPtr sounDraw, IntPtr data, uint datalen)
+        {
+            unsafe
+            {
+                uint count; //Does this need to be outside the lock? AM
+
+                lock (_syncObject)
+                {
+
+                    if (_circularBuffer.Size == 0)
+                        return RESULT.OK;
+
+                    var stereo32BitBuffer = (float*)data.ToPointer();
+                    for (count = 0; count < (datalen >> 2); count++) //WTF does this do AM
+                    {
+                        if (_circularBuffer.Size == 0)
+                            break;
+
+                        *stereo32BitBuffer++ = _circularBuffer.Get();
+                    }
+
+                }
+            }
+            return RESULT.OK;
+        }
+    }
 }
